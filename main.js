72b8f73c6915a3efdcae50195d8430f0d5990105
Seth Garrison <sethg@l4digital.com>
2017-11-27 15:01:34 -0800

    show window when ready

--- main.js
+++ main.js
@@ -2,6 +2,16 @@ const {app, BrowserWindow} = require('electron');
 const path = require('path');
 
 app.on('ready', () => {
-    const window = new BrowserWindow();
+    // initialize window to not show
+    const window = new BrowserWindow({
+        show: false
+    });
+
+    // load url to window
     window.loadURL(path.join('file://', __dirname, 'index.html'));
+
+    // when window is ready and all files are loaded, show window
+    window.on('ready-to-show', () => {
+        window.show();
+    });
 });
