5cefcdfff1c23f44f1596da7a26d7841ab69b09e
Seth Garrison <sethg@l4digital.com>
2017-11-27 15:09:25 -0800

    adding a simple modal window

--- main.js
+++ main.js
@@ -3,11 +3,22 @@ const {app, BrowserWindow} = require('electron');
 app.on('ready', () => {
     // initialize one parent window
     const parent = new BrowserWindow();
-    // create a new child window using "parent" prop
-    const child = new BrowserWindow({parent: parent, width: 100, height: 100});
+
+    // create a new modal window using parent & modal props
+    const child = new BrowserWindow({
+        parent: parent,
+        modal: true,
+        show: false
+    });
+
+    // use load url to show web page
+    child.loadURL('https://github.com');
 
     // parent will show beneath
     parent.show();
-    // child will show on top
-    child.show();
+
+    // when child is ready, show
+    child.once('ready-to-show', () => {
+        child.show()
+    })
 });
