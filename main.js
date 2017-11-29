cab79ce1fef2d80b804375f2c3745b6ca61a7b07
Seth Garrison <sethg@l4digital.com>
2017-11-29 14:59:26 -0800

    simple print to pdf example

--- main.js
+++ main.js
@@ -1,13 +1,23 @@
-const {app, BrowserWindow} = require('electron');
+const {app, BrowserWindow, ipcMain, shell} = require('electron');
+const os = require('os');
+const fs = require('fs');
+const path = require('path');
 
 app.on('ready', () => {
-    const window = new BrowserWindow({
-        show: false
-    });
+    const window = new BrowserWindow();
 
     window.loadURL(`file://${__dirname}/index.html`);
 
-    window.on('ready-to-show', () => {
-        window.show();
+    ipcMain.on('print-to-pdf', (event) => {
+        const pdfPath = path.join(os.tmpdir(), 'prind.pdf');
+        const win = BrowserWindow.fromWebContents(event.sender);
+        win.webContents.printToPDF({}, (err, data) => {
+            if (err) throw err;
+            fs.writeFile(pdfPath, data, (err) => {
+                if (err) throw err;
+                shell.openExternal(`file://${pdfPath}`);
+                event.sender.send('wrote-pdf', pdfPath);
+            });
+        });
     });
 });
--- renderer.js
+++ renderer.js
@@ -1,36 +1,12 @@
-const {remote} = require('electron');
-const {BrowserWindow} = remote;
-const currentWindow = remote.getCurrentWindow();
+const ipc = require('electron').ipcRenderer;
 
-const newWindow = document.querySelector('#new-window');
+const printPDFBtn = document.getElementById('print-pdf');
 
-newWindow.addEventListener('click', () => {
-    const win = new BrowserWindow({x: 0, y: 0});
-    win.loadURL(`file://${__dirname}/index.html`);
+printPDFBtn.addEventListener('click', function (event) {
+    ipc.send('print-to-pdf');
 });
 
-function onBlur(){
-    document.body.style = 'opacity: 0.2;';
-}
-
-function onFocus(){
-    document.body.style = 'opacity: 1;';
-}
-
-function updateWindowInfo () {
-    const manageWindowReply = document.getElementById('window-state');
-    const message = `Size: ${currentWindow.getSize()} Position: ${currentWindow.getPosition()}`;
-    manageWindowReply.innerText = message
-}
-
-currentWindow.on('blur', onBlur);
-currentWindow.on('focus', onFocus);
-currentWindow.on('resize', updateWindowInfo);
-currentWindow.on('move', updateWindowInfo);
-
-window.addEventListener('beforeunload', () => {
-    currentWindow.removeListener('blur', onBlur);
-    currentWindow.removeListener('focus', onFocus);
-    currentWindow.removeListener('move', updateWindowInfo);
-    currentWindow.removeListener('resize', updateWindowInfo);
+ipc.on('wrote-pdf', function (event, path) {
+    const message = `Wrote PDF to: ${path}`;
+    document.getElementById('pdf-path').innerHTML = message;
 });
