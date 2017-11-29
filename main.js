12f5328ea2946172988d9a5a82e44d30fa4976ec
Seth Garrison <sethg@l4digital.com>
2017-11-28 16:28:33 -0800

    simple window state

--- main.js
+++ main.js
@@ -1,9 +1,4 @@
-const {app, BrowserWindow, ipcMain, Menu, MenuItem} = require('electron');
-
-const menu = new Menu();
-menu.append(new MenuItem({ label: 'Hello' }));
-menu.append(new MenuItem({ type: 'separator' }));
-menu.append(new MenuItem({ label: 'Electron', type: 'checkbox', checked: true }));
+const {app, BrowserWindow} = require('electron');
 
 app.on('ready', () => {
     const window = new BrowserWindow({
@@ -14,15 +9,5 @@ app.on('ready', () => {
 
     window.on('ready-to-show', () => {
         window.show();
-        window.openDevTools();
-    });
-
-    ipcMain.on('show-menu', (event) => {
-        const win = BrowserWindow.fromWebContents(event.sender);
-        menu.popup(win);
-    });
-
-    window.webContents.on('context-menu', (event, params) => {
-        menu.popup(window, params.x, params.y)
     });
 });
--- renderer.js
+++ renderer.js
@@ -1,7 +1,36 @@
-const {ipcRenderer} = require('electron');
-require('devtron').install();
-const sendMessageButton = document.querySelector('#show-menu');
+const {remote} = require('electron');
+const {BrowserWindow} = remote;
+const currentWindow = remote.getCurrentWindow();
 
-sendMessageButton.addEventListener('click', ()=> {
-    ipcRenderer.send('show-menu');
+const newWindow = document.querySelector('#new-window');
+
+newWindow.addEventListener('click', () => {
+    const win = new BrowserWindow({x: 0, y: 0});
+    win.loadURL(`file://${__dirname}/index.html`);
+});
+
+function onBlur(){
+    document.body.style = 'opacity: 0.2;';
+}
+
+function onFocus(){
+    document.body.style = 'opacity: 1;';
+}
+
+function updateWindowInfo () {
+    const manageWindowReply = document.getElementById('window-state');
+    const message = `Size: ${currentWindow.getSize()} Position: ${currentWindow.getPosition()}`;
+    manageWindowReply.innerText = message
+}
+
+currentWindow.on('blur', onBlur);
+currentWindow.on('focus', onFocus);
+currentWindow.on('resize', updateWindowInfo);
+currentWindow.on('move', updateWindowInfo);
+
+window.addEventListener('beforeunload', () => {
+    currentWindow.removeListener('blur', onBlur);
+    currentWindow.removeListener('focus', onFocus);
+    currentWindow.removeListener('move', updateWindowInfo);
+    currentWindow.removeListener('resize', updateWindowInfo);
 });
